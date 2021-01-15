2d0b43aee1dc1205b26ea366b8f8d60c51f3f1d5
Yoon_Koh <bear04012@gmail.com>
2021-01-15 17:47:12 -0500

    First time using reactive variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { useReactiveVar } from '@apollo/client';
 import React from 'react';
+import { LoggedInRouter } from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
+import { isLoggedInVar } from './apollo';
 
 function App() {
-  return (
-    <LoggedOutRouter />
-  );
+  // const { data: { isLoggedIn } } = useQuery(IS_LOGGED_IN);
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,3 +1,14 @@
 import React from 'react';
+import { isLoggedInVar } from '../apollo';
 
-export const LoggedInRouter = () => <span>Logged Out</span>;
+export const LoggedInRouter = () =>  {
+  const onClick = () => {
+    isLoggedInVar(false);
+  }
+  return (
+    <div>
+      <h1>logged in</h1>
+      <button onClick={onClick}>Click to log out</button>
+    </div>
+  );
+}
