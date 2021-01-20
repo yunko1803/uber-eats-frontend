771e32803784d1de6afed21fc0212c8dd2e8ad15
Yoon_Koh <bear04012@gmail.com>
2021-01-20 14:29:16 -0500

    Demo react hook

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { isLoggedInVar } from '../apollo';
 
 export const LoggedOutRouter = () =>  {
-  const onClick = () => {
-    isLoggedInVar(true);
+  const { register, watch, handleSubmit, errors } = useForm();
+  const onSubmit = () => {
+    console.log(watch());
   }
+  const onInvalid = () => {
+    console.log('cant create an account');
+  }
+  console.log('error: ',errors);
   return (
     <div>
       <h1>logged out</h1>
-      <button onClick={onClick}>Click to login</button>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            ref={register({
+              required: 'This is required',
+              validate: (email: string) => email.includes('@gmail.com'),
+            })}
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <input
+            ref={register({
+              required: true
+            })}
+            name="password"
+            type="password"
+            required
+            placeholder="password"
+          />
+        </div>
+        <button className="bg-yellow-300 text-white">Submit</button>
+      </form>
     </div>
   );
 }
