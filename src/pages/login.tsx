8d32965c21fa862b8a08e70eb797867e18cea02f
Yoon_Koh <bear04012@gmail.com>
2021-01-25 15:50:03 -0500

    Create login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export const Login = () => {
-  return <span>Login</span>
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
+        <h3 className="text-3xl text-gray-800">Log In</h3>
+        <form className="flex flex-col mt-5 px-5">
+          <input
+            placeholder="email"
+            className="bg-gray-200 shadow-inner focus:outline-none mb-3 py-3 px-5 rounded-lg border-2 focus:border-opacity-50 focus:border-green-600"
+          />
+          <input
+            placeholder="password"
+            className="bg-gray-200 shadow-inner focus:outline-none mb-3 py-3 px-5 rounded-lg border-2 focus:border-opacity-50 focus:border-green-600"
+          />
+          <button className="py-3 px-5 bg-gray-800 text-white text-lg rounded-lg focus:outline-none hover:opacity-90">Log In</button>
+        </form>
+      </div>
+    </div>
+  )
 };
